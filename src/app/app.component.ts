487116932ae12eb754d195a3cdf355c68004dd2b
Rohan Raj <rohan@gmail.com>
2022-09-02 16:09:47 +0530

    first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'To Do List';
+  list:any[] = [];
+  addTask(item:string){
+    console.warn(this.list)
+    this.list.push({id:this.list.length,name:item});
+  }
+  removeTask(id:number){
+    this.list = this.list.filter(item=>item.id!==id);
+
+  }
 }
